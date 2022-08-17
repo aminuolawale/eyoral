d9ab88047485773e7a0ce1952cef9d51b8c5e917
mohammedaminu <aminuolawalekan@gmail.com>
2022-08-17 22:49:44 +0200

    Initial commit

---
+++ src/main/java/com/laroey/laroye/LaroyeApplication.java
@@ -0,0 +1,13 @@
+package com.laroey.laroye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaroyeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaroyeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laroey/laroye/LaroyeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laroey.laroye;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaroyeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
